64e3674ac2e069cf344eb727d2ab71e54c531065
b166erobot <bernard0freitas@outlook.com>
2024-03-01 09:58:21 -0300

    Requisito 1 satisfeito

--- formatedBookNames.js
+++ formatedBookNames.js
@@ -1,7 +1,13 @@
-// const { books } = require('./data/library');
+const { books } = require('./data/library');
 
 const formatedBookNames = () => {
   // escreva seu código aqui
+  const stringsArray = books.map(
+    ({name, genre, author: {name: authorName}}) => {
+      return `${name} - ${genre} - ${authorName}`
+    }
+  );
+  return stringsArray;
 };
 
 module.exports = { formatedBookNames };
