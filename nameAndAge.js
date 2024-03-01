f9b89243ebaa0b36c3e1045aade3beafc7ef0702
b166erobot <bernard0freitas@outlook.com>
2024-03-01 10:45:55 -0300

    Requisito 3 satisfeito

--- nameAndAge.js
+++ nameAndAge.js
@@ -1,7 +1,23 @@
-// const { books } = require('./data/library');
+const { books } = require('./data/library');
 
 const nameAndAge = () => {
   // escreva seu código aqui
+  let authorsArray = books.map(
+    ({author: {name, birthYear}, releaseYear}) => {
+      return {
+        author: name,
+        age: releaseYear - birthYear,
+      };
+    }
+  );
+  authorsArray = authorsArray.sort((book1, book2) => {
+    if (book1.age > book2.age) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+  return authorsArray;
 };
 
 module.exports = { nameAndAge };
